776cd4930cc1eb522cec0cd285916a5115489b66
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-12-12 14:37:47 +0100

    Add day 04 part 01

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -3,10 +3,29 @@ import run from "aocrunner";
 const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  
+  const input = parseInput(rawInput)
+    .split("\n")
+    .map((i) => {
+      const [winnerPart, cardsPart] = i.split("|");
+      const extractNumbers = (str: string) =>
+        str.trim().split(" ").map(Number).filter(Boolean);
+      return {
+        winners: extractNumbers(winnerPart.split(":")[1]),
+        cards: extractNumbers(cardsPart),
+      };
+    });
 
-  return;
+  let result = 0;
+
+  input.forEach(({ winners, cards }) => {
+    let points = 0;
+
+    cards.forEach((i) => (points += winners.includes(i) ? 1 : 0));
+
+    points > 1 ? (result += 2 ** (points - 1)) : (result += points);
+  });
+
+  return result;
 };
 
 const part2 = (rawInput: string) => {
