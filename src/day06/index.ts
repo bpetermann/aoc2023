1d0178a51e40d2c2ebdd94334266e692720225bd
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-12-13 13:57:37 +0100

    Cleanup

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -3,29 +3,18 @@ import run from "aocrunner";
 const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  const times = input
-    .split("\n")[0]
-    .split(":")[1]
-    .split(" ")
-    .map(Number)
-    .filter(Boolean);
-  const distances = input
-    .split("\n")[1]
-    .split(":")[1]
-    .split(" ")
-    .map(Number)
-    .filter(Boolean);
+  let [t, d] = parseInput(rawInput).split("\n");
+  const times = t.split(":")[1].split(" ").map(Number).filter(Boolean);
+  const distances = d.split(":")[1].split(" ").map(Number).filter(Boolean);
 
   let total = 1;
 
   times.map((time, index) => {
-    const distanceToBeat = distances[index];
     let wins = 0;
 
-    new Array(time).fill(time).forEach((time, index) => {
-      wins += (time - index) * index > distanceToBeat ? 1 : 0;
-    });
+    for (let i = 0; i < time; i++) {
+      wins += (time - i) * i > distances[index] ? 1 : 0;
+    }
 
     total *= wins;
   });
@@ -34,26 +23,17 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  const times = [+input.split("\n")[0].split(":")[1].split(" ").join("")];
-  const distances = [+input.split("\n")[1].split(":")[1].split(" ").join("")];
+  let [t, d] = parseInput(rawInput).split("\n");
+  const times = [+t.split(":")[1].split(" ").join("")];
+  const distances = [+d.split(":")[1].split(" ").join("")];
 
   let total = 1;
 
   times.map((time, index) => {
     let wins = 0;
-    let distanceEnchancer = 0;
-    let timeLeft = time;
 
     for (let i = 0; i < time; i++) {
-      timeLeft -= i;
-      distanceEnchancer += i;
-
-      if (timeLeft * distanceEnchancer > distances[index]) {
-        wins += 1;
-      }
-      timeLeft = time;
-      distanceEnchancer = 0;
+      wins += (time - i) * i > distances[index] ? 1 : 0;
     }
 
     total *= wins;
