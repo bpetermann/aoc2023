b59da00a053daf2dc042aabd65fb93ed050466e3
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-12-15 23:36:44 +0100

    Add day 08 part 02

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -30,14 +30,54 @@ const part1 = (rawInput: string) => {
 
     index = index + 1 < instructions.length ? index + 1 : 0;
 
-    steps += 1;
+    steps++;
   }
 
   return steps;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const [i, n] = parseInput(rawInput).split("\n\n");
+  const instructions = i.split("");
+  const nodes = n.split("\n").map((i) => ({
+    start: i.split(" ")[0],
+    L: i.split("(")[1].split(",")[0],
+    R: i.split(", ")[1].split(")")[0],
+  }));
+
+  const getNextNode = (node: string) => {
+    return nodes.find((i) => i.start === node) as {
+      start: string;
+      L: string;
+      R: string;
+    };
+  };
+
+  const lcm = (...arr: number[]) => {
+    const gcd = (x: number, y: number): number => (!y ? x : gcd(y, x % y));
+    const _lcm = (x: number, y: number): number => (x * y) / gcd(x, y);
+    return [...arr].reduce((a, b) => _lcm(a, b));
+  };
+
+  let allStartingNodes = nodes.filter(({ start }) => start[2] === "A");
+
+  const stepsToZ = allStartingNodes.map((i) => {
+    let start = i;
+    let index = 0;
+    let steps = 0;
+
+    while (start.start[2] !== "Z") {
+      start = getNextNode(instructions[index] === "L" ? start.L : start.R);
+
+      index = index + 1 < instructions.length ? index + 1 : 0;
+
+      steps++;
+    }
+
+    return steps;
+  });
+
+  return lcm(...stepsToZ);
 };
 
 run({
