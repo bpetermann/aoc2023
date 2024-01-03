7c4267f013e7b67037afbc9de756d1808679d94e
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-01-03 14:40:08 +0100

    Add day 11 part 02

--- src/day11/index.ts
+++ src/day11/index.ts
@@ -43,6 +43,7 @@ const part1 = (rawInput: string) => {
   const findShortestPath = (p1: number, p2: number) => {
     const [x1, y1] = getGalaxyPosition(p1);
     const [x2, y2] = getGalaxyPosition(p2);
+
     return Math.abs(x1 - x2) + Math.abs(y1 - y2);
   };
 
@@ -58,9 +59,87 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput)
+    .split("\n")
+    .map((i) => i.split(""));
 
-  return;
+  const expandedInput: (number | string)[][] = [];
+  const expansionRate = 1000000;
+  const EXPAND_ROW = "EXPAND_ROW";
+  const EXPAND_COLUMN = "EXPAND_COLUMN";
+  let galaxyCount = 0;
+
+  input.forEach((i) => {
+    !i.includes("#")
+      ? expandedInput.push([EXPAND_ROW])
+      : expandedInput.push(
+          i
+            .map((i) => (i === "." ? i : ++galaxyCount))
+            .flatMap((i, idx) =>
+              input.every((i) => i[idx] === ".") ? [i, EXPAND_COLUMN] : i,
+            ),
+        );
+  });
+
+  let galaxyPositionMap: Map<number, [number, number]> = new Map();
+
+  const getGalaxyPosition = (point: number): [number, number] => {
+    if (galaxyPositionMap.has(point))
+      return galaxyPositionMap.get(point) as [number, number];
+
+    let galaxyPosition: [number, number] = [0, 0];
+
+    expandedInput.find((row, i) => {
+      const j = row.indexOf(point);
+      if (j !== -1) galaxyPosition = [i, j];
+    });
+
+    galaxyPositionMap.set(point, galaxyPosition);
+
+    return galaxyPosition;
+  };
+
+  const getExpandedRows = (start: number, end: number) => {
+    const expandedRowsFound = expandedInput
+      .slice(start, end)
+      .filter((i) => i[0] === EXPAND_ROW).length;
+    return expandedRowsFound * expansionRate - expandedRowsFound;
+  };
+
+  const sortPoints = (p1: number, p2: number) =>
+    p1 < p2 ? [p1, p2] : [p2, p1];
+
+  const getExpandedColumns = (row: number, p1: number, p2: number) => {
+    const [start, end] = sortPoints(p1, p2);
+
+    const columnsFound = expandedInput[row]
+      .slice(start, end)
+      .filter((i) => i === EXPAND_COLUMN).length;
+
+    return columnsFound * expansionRate - columnsFound * 2;
+  };
+
+  const findShortestPath = (p1: number, p2: number) => {
+    const [x1, y1] = getGalaxyPosition(p1);
+    const [x2, y2] = getGalaxyPosition(p2);
+    
+    return (
+      Math.abs(x1 - x2) +
+      Math.abs(y1 - y2) +
+      getExpandedColumns(x2, y1, y2) +
+      getExpandedRows(x1, x2)
+    );
+  };
+
+  let shortestPaths = 0;
+
+  for (let i = 1; i <= galaxyCount; i++) {
+    for (let j = i + 1; j <= galaxyCount; j++) {
+      shortestPaths += findShortestPath(i, j);
+    }
+  }
+
+  return shortestPaths;
 };
 
 run({
