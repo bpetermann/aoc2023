5406e6fe73cff316de7e351671f9f4045d6a88bf
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-01-09 21:49:10 +0100

    Add day 15 part 02

--- README.md
+++ README.md
@@ -25,7 +25,7 @@
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%86/yellow)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
-[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: 8.598ms
 
 ```
 Day 15
-Time part 1: 2.807ms
-Time part 2: -
-Both parts: 2.807ms
+Time part 1: 2.817ms
+Time part 2: 9.286ms
+Both parts: 12.103ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 22/50
-Total time: 537.67ms
+Total stars: 23/50
+Total time: 546.966ms
 ```
 
 <!--/RESULTS-->
--- src/day15/index.ts
+++ src/day15/index.ts
@@ -21,9 +21,77 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput).split(",");
 
-  return;
+  const getHashValue = (input: string) => {
+    let currenValue = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      currenValue += input[i].charCodeAt(0);
+      currenValue *= 17;
+      currenValue = currenValue % 256;
+    }
+
+    return currenValue;
+  };
+
+  const boxes: { [k: number]: string[][] } = {};
+
+  const getOperation = (input: string) => {
+    return input.includes("=") ? "=" : "-";
+  };
+
+  const getBox = (hash: number) => {
+    return boxes[hash];
+  };
+
+  input.forEach((i) => {
+    const operation = getOperation(i);
+    const [box, label] = i.split(operation);
+    const hash = getHashValue(box);
+    const currentBox = getBox(hash);
+
+    if (operation === "=") {
+      if (!currentBox) {
+        boxes[hash] = [[box, label]];
+        return;
+      }
+
+      const lensIndex = currentBox.findIndex((i) => i[0] === box);
+
+      if (lensIndex > -1) {
+        currentBox[lensIndex][1] = label;
+        return;
+      }
+      currentBox.push([box, label]);
+    } else {
+      if (!currentBox) {
+        return;
+      }
+      const lensIndex = currentBox.findIndex((i) => i[0] === box);
+      if (lensIndex < 0) {
+        return;
+      }
+      boxes[hash] = currentBox
+        .slice(0, lensIndex)
+        .concat(currentBox.slice(lensIndex + 1));
+    }
+  });
+
+  const getTotalPower = (boxes: { [k: number]: string[][] }) => {
+    let result = 0;
+
+    for (const hash in boxes) {
+      const box = boxes[hash];
+      box.forEach((i, idx) => {
+        result += (+hash + 1) * (idx + 1) * +i[1];
+      });
+    }
+
+    return result;
+  };
+
+  return getTotalPower(boxes);
 };
 
 run({
